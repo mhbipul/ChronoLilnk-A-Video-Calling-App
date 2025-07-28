7fe0d77d364a6fbddc743a8b17ff2cbdc6dd05e5
Bipul <mhbipul99bd@gmail.com>
2025-07-28 23:09:25 +0600

    Project Initialization

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import  "dotenv/config"
+import authRoutes from "./routes/auth.routes.js"
+import { connectDB } from "./lib/db.js";
+
+
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json())
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+    connectDB();
+    
+})
