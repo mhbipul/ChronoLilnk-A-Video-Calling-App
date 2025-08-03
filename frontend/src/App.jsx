514101851acbe81596b3f4887f6f1e52250c33fb
Bipul <mhbipul99bd@gmail.com>
2025-08-03 23:50:15 +0600

    Signup completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import HomePage from "./pages/HomePage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
 import ChatPage from "./pages/ChatPage.jsx"
@@ -15,7 +15,7 @@ import { axiosInstance } from './lib/axios.js'
 const App = () => {
   //axios
   //set up  react query / transtack
-  const {data,r} = useQuery({queryKey:['todos'],
+  const {data:authData,isLoading,error} = useQuery({queryKey:['authUser'],
     queryFn : async()=>{
       const res = await axiosInstance.get("/auth/me")
       return res.data 
@@ -23,20 +23,20 @@ const App = () => {
     //using retry-> false for loading only once..
     retry : false, // auth check
   });
-  console.log(data);
+  const authUser = authData?.user 
 
   return (
     <div className=' h-screen' data-theme="night">
       <Toaster />
       
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/call' element={<CallPage />} />
-        <Route path='/chat' element={<ChatPage />} />
-        <Route path='/notifications' element={<NotificationsPage />} />
-        <Route path='/onboarding' element={<OnboardingPage />} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/call' element={authUser ? <CallPage />: <Navigate to='/login'/>} />
+        <Route path='/chat' element={authUser ?<ChatPage /> : <Navigate to='/login'/>} />
+        <Route path='/notifications' element={authUser ? <NotificationsPage />: <Navigate to='/login' />} />
+        <Route path='/onboarding' element={authUser ?<OnboardingPage />: <Navigate to='/login' />} />
 
       </Routes>
       
