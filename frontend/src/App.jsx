356b4bf1b7de1f4e81542d04202dfd9e2d31c935
Bipul <mhbipul99bd@gmail.com>
2025-08-10 19:12:58 +0600

    Chat page compelete + backend update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,12 @@ const App = () => {
         <Route path='/signup' element={!isAuthenticated ? <SignUpPage /> : <Navigate to={isOnboarded ? '/' : '/onboarding'} />} />
         <Route path='/login' element={!isAuthenticated ? <LoginPage /> : <Navigate to={isOnboarded ? '/' : '/onboarding'} />} />
         <Route path='/call' element={isAuthenticated ? <CallPage />: <Navigate to='/login'/>} />
-        <Route path='/chat' element={isAuthenticated ?<ChatPage /> : <Navigate to='/login'/>} />
+
+        <Route path='/chat/:id' element={isAuthenticated && isOnboarded ?(<Layout showSidebar={false}>
+          <ChatPage />
+        </Layout>) :( <Navigate to={!isAuthenticated ? '/login' : "/onboarding"}/>)} />
+
+
         <Route path='/notifications' element={isAuthenticated && isOnboarded ? (
           <Layout showSidebar={true}>
             <NotificationsPage />
